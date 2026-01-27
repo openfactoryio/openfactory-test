eada362de5a6ce8c312c6d2b491730afddc970d4
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2026-01-27 21:44:04 +0000

    Initial commit

---
+++ opcua_test_change_var.py
@@ -0,0 +1,27 @@
+import time
+import openfactory.config as config
+from openfactory import OpenFactory
+from openfactory.assets import Asset
+from openfactory.kafka import KSQLDBClient
+
+# --- Setup ---
+ksql = KSQLDBClient(config.KSQLDB_URL)
+ofa = OpenFactory(ksqlClient=KSQLDBClient(config.KSQLDB_URL))
+
+asset = Asset('OPCUA-SENSOR-001', ksqlClient=ksql)
+
+print(asset.temp)
+print(asset.temp_unit)
+
+asset.temp_unit = 'K'
+time.sleep(0.1)
+
+print(asset.temp_unit)
+
+def on_sample(msg_key, msg_value):
+    print(msg_key, msg_value['VALUE'])
+    print(msg_value)
+
+asset.subscribe_to_attribute('temp', on_sample)
+
+time.sleep(10)
