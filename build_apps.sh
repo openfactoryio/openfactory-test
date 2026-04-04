3d008dd5dfe9b58e11cb6bd08cb8c790dafabe85
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2026-04-04 15:10:25 +0000

    Use correct version

--- build_apps.sh
+++ build_apps.sh
@@ -2,5 +2,5 @@
 
 # Build demo-app
 cd ressources/apps/demo
-docker build --build-arg OFA_VERSION="v$OPENFACTORY_VERSION" -t demo-app -f Dockerfile .
+docker build --build-arg OFA_VERSION="$OPENFACTORY_VERSION" -t demo-app -f Dockerfile .
 cd /workspaces/openfactory-test
