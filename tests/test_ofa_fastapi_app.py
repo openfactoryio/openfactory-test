360b6977fd305b8ef9257407ea02c1ae935ff4fe
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2026-05-05 01:32:14 +0000

    Initial commit

---
+++ tests/test_ofa_fastapi_app.py
@@ -0,0 +1,32 @@
+import unittest
+import requests
+
+
+BASE_URL = "http://demo-fastapi-app-demo.openfactory.local"
+
+
+class TestFastAPIEndpoints(unittest.TestCase):
+    """
+    Test endpoints of an OpenFactoryFastAPI App
+    """
+
+    def test_root_endpoint(self):
+        response = requests.get(f"{BASE_URL}/")
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertIn("status", data)
+
+    def test_move_endpoint(self):
+        payload = {
+            "x": 1.5,
+            "y": 2.5
+        }
+
+        response = requests.post(f"{BASE_URL}/move", params=payload)
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertEqual(data.get("message"), "moving")
